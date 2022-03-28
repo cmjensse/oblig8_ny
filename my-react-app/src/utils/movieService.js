7561521a7a5b317b5c64135e480dbd13927be671
cmjensse <ch4rlotteje@gmail.com>
2022-03-28 16:00:11 +0200

    Add files via upload

---
+++ my-react-app/src/utils/movieService.js
@@ -0,0 +1,21 @@
+import client from "./client";
+
+const eventFields = `
+    movie, 
+    titlename, 
+    'slug': slug.current,
+    "actor": actor->title,
+`;
+
+export const getEvents = async () => {
+  const data = await client.fetch(`*[_type == "movie"]${eventFields}`);
+  return data;
+};
+
+export const getEvent = async (slug) => {
+  const data = await client.fetch(
+    `*[_type == "movie" && slug.current == $slug]{${eventFields}}`,
+    { slug }
+  );
+  return data;
+};
